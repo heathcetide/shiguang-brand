19d59fd9cc2726b61897226c551ef94f13b1350d
heathcetide <154225089+heathcetide@users.noreply.github.com>
2024-12-18 09:32:52 +0800

    去除

--- src/main/java/com/foodrecord/common/utils/AliOssUtil.java
+++ src/main/java/com/foodrecord/common/utils/AliOssUtil.java
@@ -6,19 +6,22 @@ import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.OSSException;
 import com.aliyun.oss.model.PutObjectRequest;
 import com.aliyun.oss.model.PutObjectResult;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.InputStream;
 
 public class AliOssUtil {
+    @Value("${oss.client.keyId}")
+    private static String ACCESS_KEY_ID;
 
-    // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
-    private static final String ENDPOINT = "https://oss-cn-beijing.aliyuncs.com";
-    // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
-    //EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
-    private static final String ACCESS_KEY_ID="LTAI5tPao4rwMV6qSiUkcmvB";
-    private static final String ACCESS_KEY_SECRET="iWU8LraTYPHuuyjo3Wh2SPWXCJ1Y5T";
-    // 填写Bucket名称，例如examplebucket。
-    private static final String BUCKET_NAME = "cetide";
+    @Value("${oss.client.keySecret}")
+    private static String ACCESS_KEY_SECRET;
+
+    @Value("${oss.client.endPoint}")
+    private static String ENDPOINT;
+
+    @Value("${oss.client.bucket}")
+    private static String BUCKET_NAME;
 
     public static String uploadFile(String objectName, InputStream in) throws Exception {
 
